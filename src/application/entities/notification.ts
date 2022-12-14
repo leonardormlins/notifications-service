5cd3638217f259e0689dfb9a6bd0f0de0a3cb723
leonardormlins <leonardormlins@gmail.com>
2022-12-14 00:37:24 -0300

    feat: create notification endpoint

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
@@ -10,13 +11,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public get id(): string {
+    return this._id;
   }
 
   public set recipientId(recipientId: string) {
@@ -25,6 +32,14 @@ export class Notification {
 
   public get recipientId(): string {
     return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
   }
 
   public set category(category: string) {
--- src/infra/http/dtos/create-notification-body.ts
+++ src/infra/http/dtos/create-notification-body.ts
@@ -1,14 +1,14 @@
-import { IsNotEmpty, IsUUID, Length } from "class-validator";
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationBody {
   @IsNotEmpty()
   @IsUUID()
   recipientId: string;
-  
+
   @IsNotEmpty()
   @Length(5, 240)
   content: string;
-  
+
   @IsNotEmpty()
-  category: string
+  category: string;
 }
