5ebcbe411be3ed3823aeede6b0e3a6e255cc7bdc
llins <leonardo@bizpoke.com.br>
2022-12-13 22:07:41 -0300

    feat: add notification entity and tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './infra/app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from './infra/prisma.service';
 
 @Module({
   imports: [],
