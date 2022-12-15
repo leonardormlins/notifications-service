770b0154f18bed80877135a0b6bd047735080a86
leonardormlins <leonardormlins@gmail.com>
2022-12-14 23:09:39 -0300

    feat: read and unread notification use case

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -37,4 +37,10 @@ export class InMemoryNotificationsRepository
       (notification) => notification.recipientId === recipientId,
     ).length;
   }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
+  }
 }
