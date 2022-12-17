e5147854e4ffda72589dc44782f2b8249e60ef85
leonardormlins <leonardormlins@gmail.com>
2022-12-16 23:53:18 -0300

    feature: send notification by kafka message

--- src/infra/messaging/kafka/controllers/notifications.controller.ts
+++ src/infra/messaging/kafka/controllers/notifications.controller.ts
@@ -1,10 +1,25 @@
+import { SendNotification } from '@application/use-cases/send-notification';
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 
+interface SendNotificationPayload {
+  content: string;
+  category: string;
+  recipientId: string;
+}
+
 @Controller()
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @EventPattern('notifications.send-notification')
-  async handleSendNotification(@Payload() content: any) {
-    console.log(content);
+  async handleSendNotification(@Payload() body: SendNotificationPayload) {
+    const { recipientId, content, category } = body;
+
+    await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
   }
 }
